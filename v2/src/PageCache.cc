2fa4b44b1a5fe9c3206a1fc16f927c46b13c1edf
chioca <kraistine21@gmail.com>
2025-10-17 20:45:14 -0700

    添加中心缓存向系统申请内存以及回收Span

--- v2/src/PageCache.cc
+++ v2/src/PageCache.cc
@@ -1,7 +1,10 @@
 #include "PageCache.h"
 
-#include "CentralCache.h"
+#include <sys/mman.h>
 
+#include <cstring>
+
+#include "CentralCache.h"
 namespace memory_pool {
 void* PageCache::allocateSpan(size_t numPages) {
   std::lock_guard<std::mutex> lock(mutex_);
@@ -47,6 +50,62 @@ void* PageCache::allocateSpan(size_t numPages) {
   span->next = nullptr;
 
   spanMap_[memory] = span;
+  return memory;
+}
+void PageCache::deallocateSpan(void* ptr, size_t numPages) {
+  std::lock_guard<std::mutex> lock(mutex_);
+
+  auto it = spanMap_.find(ptr);
+  if (it == spanMap_.end()) return;
+
+  // 查找下一块span
+  Span* span = it->second;
+  void* nextAddr = static_cast<char*>(ptr) + numPages * PAGE_SIZE;
+  auto nextIt = spanMap_.find(nextAddr);
+
+  // 当下一块span在空闲span链表中 从中拿出
+  if (nextIt != spanMap_.end()) {
+    Span* nextSpan = nextIt->second;
+
+    bool found = false;
+    auto& nextList = freeSpans_[nextSpan->numPages];
+
+    // 下一块span为链头 直接拿出
+    if (nextList == nextSpan) {
+      nextList = nextSpan->next;
+      found = true;
+    } else if (nextList) {
+      // 下一块span在链中间
+      Span* prev = nextList;
+      while (prev->next) {
+        if (prev->next == nextSpan) {
+          prev->next = nextSpan->next;
+          found = true;
+          break;
+        }
+        prev = prev->next;
+      }
+    }
+
+    // 在空闲链表中找到nextSpan 才对齐进行合并
+    if (found) {
+      span->numPages += nextSpan->numPages;
+      spanMap_.erase(nextAddr);
+      delete nextSpan;
+    }
+
+    auto& list = freeSpans_[span->numPages];
+    span->next = list;
+    list = span;
+  }
+}
+void* PageCache::systemAlloc(size_t numPages) {
+  size_t size = numPages * PAGE_SIZE;
+  void* memory = mmap(nullptr, size, PROT_READ | PROT_WRITE,
+                      MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+
+  if (memory == MAP_FAILED) return nullptr;
+  memset(memory, 0, size);
   return memory;
 }
 }  // namespace memory_pool
